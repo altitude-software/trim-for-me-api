eab25f29f772cb59723d542cdd5db4ad85e4e2e0
Jose Cornejo <jose.cornejo.lupa@gmail.com>
2026-05-11 22:29:53 -0500

    se hizo un fix sobre el endpoint de login

--- src/modules/users/application/use-cases/login-user.use-case.ts
+++ src/modules/users/application/use-cases/login-user.use-case.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Email } from '../../domain/value-objects/email.vo';
 import { LoginUserDto } from '../dtos/login-user.dto';
-import { UserResponseDto } from '../dtos/user-response.dto';
+import { LoginResponseDto } from '../dtos/login-response.dto';
 
 import type { IUserRepository } from '../../domain/repositories/user.repository';
 import type { IPasswordHasher } from '../../domain/services/password-hasher.interface';
@@ -19,9 +20,10 @@ export class LoginUserUseCase {
         private readonly userRepository: IUserRepository,
         @Inject('IPasswordHasher')
         private readonly passwordHasher: IPasswordHasher,
+        private readonly jwtService: JwtService,
     ) { }
 
-    async execute(dto: LoginUserDto): Promise<UserResponseDto> {
+    async execute(dto: LoginUserDto): Promise<LoginResponseDto> {
         const email = new Email(dto.email);
 
         const user = await this.userRepository.findByEmail(email);
@@ -30,13 +32,7 @@ export class LoginUserUseCase {
         const passwordMatch = await this.passwordHasher.compare(dto.password, user.password);
         if (!passwordMatch) throw new InvalidCredentialsException();
 
-        return {
-            id: user.id.value,
-            email: user.email.value,
-            role: user.role,
-            name: user.name,
-            createdAt: user.createdAt,
-            updatedAt: user.updatedAt,
-        };
+        const payload = { sub: user.id.value, email: user.email.value, role: user.role };
+        return { access_token: this.jwtService.sign(payload) };
     }
 }
