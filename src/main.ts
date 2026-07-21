76b3ff89f7dd447c51327e55306e5f22e281ee57
angeljsd <angeljose9629@gmail.com>
2026-07-21 13:49:28 -0500

    Fix CORS stop bleeding

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://trim-for-me-ui-app.info-altitude-software.workers.dev',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
